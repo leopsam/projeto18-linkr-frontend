52084caaf682fa364df75bc7abbf2c3a0043c801
Leonardo Sampaio <leonardo.lps@outlook.com.br>
2023-03-08 14:06:53 -0300

    conflict resolution

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,19 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { UserProvider } from "../contexts/LinkrContext";
-import Signin from "../pages/Signin/index"
-import Signup from "../pages/Signup/index"
-import Timeline from "../pages/timelane/index"
+import Signin from "../pages/Signin/index";
+import Signup from "../pages/Signup/index";
+import Timeline from "../pages/Timeline/index";
 
 export default function App() {
   return (
     <BrowserRouter>
       <UserProvider>
-          <Routes> 
-            <Route path="/" element={<Signin />} />
-            <Route path="/signup" element={<Signup />} />  
-            <Route path="/timeline" element={<Timeline />} />                
-          </Routes> 
+        <Routes>
+          <Route path="/" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/timeline" element={<Timeline />} />
+        </Routes>
       </UserProvider>
     </BrowserRouter>
-  )
+  );
 }
--- src/pages/Timeline/index.js
+++ src/pages/Timeline/index.js
@@ -1,6 +1,5 @@
 import Header from "../../components/Header";
 import BodyStyled from "../../style/Body.styled";
-import styled from "styled-components"; /////
 
 export default function Login() {
   return (
