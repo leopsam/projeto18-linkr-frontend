d79f83f6b79afe814a46e6e9e1a33be30677f9ce
Kevin Alves <kevin.alves.dev@gmail.com>
2023-03-09 08:08:32 -0300

    Revert "Change calls to sign-in and sign-up to services handler"

--- src/contexts/LinkrContext.js
+++ src/contexts/LinkrContext.js
@@ -1,54 +1,51 @@
-import { createContext, useState } from "react";
-import { ThreeCircles, Bars } from "react-loader-spinner";
+import { createContext, useState } from 'react'
+import { ThreeCircles, Bars } from 'react-loader-spinner'
 
-export const linkrContext = createContext({});
+export const linkrContext = createContext({})
 
-export function UserProvider({ children }) {
-  const [token, setToken] = useState();
-  const [infoUser, setInfoUser] = useState({});
-  const inputFaded = "#F2F2F2";
-  const activeInput = "#FFFFFF";
-  const load = (
-    <ThreeCircles
-      height="100"
-      width="100"
-      color="#4fa94d"
-      wrapperStyle={{}}
-      wrapperClass=""
-      visible={true}
-      ariaLabel="three-circles-rotating"
-      outerCircleColor=""
-      innerCircleColor=""
-      middleCircleColor=""
+export function UserProvider( {children} ) {  
+    const REACT_APP_API_URL = process.env.REACT_APP_API_URL
+    const [token, setToken] = useState()
+    const [infoUser, setInfoUser] = useState({})
+    const inputFaded = "#F2F2F2"
+    const activeInput = "#FFFFFF"
+    const load = <ThreeCircles
+        height="100"
+        width="100"
+        color="#4fa94d"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+        ariaLabel="three-circles-rotating"
+        outerCircleColor=""
+        innerCircleColor=""
+        middleCircleColor=""
     />
-  );
-  const botaoLoading = (
-    <Bars
-      height="30"
-      width="80"
-      radius="9"
-      color="#ffffff"
-      ariaLabel="bars-loading"
-      wrapperStyle={{}}
-      wrapperClassName=""
-      visible={true}
-    />
-  );
+    const botaoLoading = <Bars 
+        height="30" 
+        width="80" 
+        radius="9"
+        color="#ffffff" 
+        ariaLabel="bars-loading"
+        wrapperStyle={{}}
+        wrapperClassName=""
+        visible={true}    
+    /> 
 
   return (
-    <linkrContext.Provider
-      value={{
+    <linkrContext.Provider value={{        
         load,
-        botaoLoading,
+        botaoLoading,       
         inputFaded,
         activeInput,
+        REACT_APP_API_URL,       
         token,
         setToken,
         infoUser,
-        setInfoUser,
-      }}
-    >
+        setInfoUser
+      
+    }}>
       {children}
     </linkrContext.Provider>
-  );
+  )
 }
--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -1,85 +1,84 @@
-import Button from "../../style/button.styled";
-import Input from "../../style/Input.styled";
+import Button from '../../style/button.styled';
+import Input from '../../style/Input.styled';
 import ContainerPage from "../../style/ContainerPage.styled";
 import SigninContainer from "../../style/ContainerSignin.styled";
 import TitleContainer from "../../style/ContainerTitle.styled";
 import LinkSignUp from "../../style/Link.styled";
-import Form from "../../style/Form.styled";
-import { linkrContext } from "../../contexts/LinkrContext";
+import Form from '../../style/Form.styled';
+import axios from "axios";
+import { linkrContext } from "../../contexts/LinkrContext"
 import { useContext, useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import { getCurrentUser, signIn } from "../../services/linkr-api";
 
 export default function Login() {
-  const { botaoLoading, activeInput, inputFaded, setInfoUser, setToken } = useContext(linkrContext);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [buttonText, setButtonText] = useState("Log In");
-  const [disabledValue, setDisabledValue] = useState("");
-  const navigate = useNavigate();
+    const { botaoLoading, activeInput, inputFaded, REACT_APP_API_URL, setInfoUser, setToken } = useContext(linkrContext)
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [buttonText, setButtonText] = useState("Log In")
+    const [disabledValue, setDisabledValue] = useState("")
+    const navegacao = useNavigate()
 
-  const signin = async (e) => {
-    e.preventDefault();
-    setButtonText(botaoLoading);
-    setDisabledValue("disabled");
+    function signin(e) {
+        e.preventDefault()
+        setButtonText(botaoLoading) 
+        setDisabledValue("disabled") 
 
-    try {
-      const {
-        data: { token },
-      } = await signIn({ email, password });
-      setToken(token);
+        const body = { email, password }
+        const url = REACT_APP_API_URL + "sign-in"
+        const promise = axios.post(url, body)
+       
+        promise.then((res) => {
+                setInfoUser(res.data.user)
+                setToken(res.data.token)  
+                navegacao('/timeline')
+            })
 
-      const { data: user } = await getCurrentUser(token);
-      setInfoUser(user);
-
-      navigate("/timeline");
-    } catch (error) {
-      setButtonText("Log In");
-      setDisabledValue("");
-      alert(error);
+            promise.catch(err => { 
+                setButtonText("Log In") 
+                setDisabledValue("")
+                alert(err)                             
+            })
     }
-  };
-
-  return (
-    <ContainerPage>
-      <TitleContainer>
-        <div>
-          <h1>linkr</h1>
-          <h2>save, share and discover the best links on the web</h2>
-        </div>
-      </TitleContainer>
-      <SigninContainer>
-        <Form onSubmit={signin}>
-          <Input
-            id="email"
-            type="email"
-            placeholder="e-mail"
-            value={email}
-            disabled={disabledValue}
-            corFundo={disabledValue ? inputFaded : activeInput}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-          <Input
-            id="password"
-            type="password"
-            placeholder="password"
-            value={password}
-            disabled={disabledValue}
-            corFundo={disabledValue ? inputFaded : activeInput}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-          <Button data-test="sign-in-submit" disabled={disabledValue} type="submit">
-            {buttonText}
-          </Button>
-        </Form>
-        <LinkSignUp>
-          <Link to={`/signup`}>
-            <p>First time? Create an account!</p>
-          </Link>
-        </LinkSignUp>
-      </SigninContainer>
-    </ContainerPage>
-  );
+        
+    return (
+        <ContainerPage>
+            <TitleContainer>
+                <div>
+                    <h1>linkr</h1>
+                    <h2>save, share and discover the best links on the web</h2>
+                </div>
+            </TitleContainer>
+            <SigninContainer>            
+                <Form onSubmit={signin}>
+                    <Input 
+                        id="email"
+                        type="email"
+                        placeholder="e-mail" 
+                        value={email} 
+                        disabled={disabledValue} 
+                        corFundo={disabledValue ? inputFaded : activeInput } 
+                        onChange={e => setEmail(e.target.value)} 
+                        required
+                    />
+                    <Input 
+                        id="password"
+                        type="password" 
+                        placeholder="password" 
+                        value={password} 
+                        disabled={disabledValue} 
+                        corFundo={disabledValue ? inputFaded : activeInput } 
+                        onChange={e => setPassword(e.target.value)}
+                        required
+                    />
+                    <Button data-test="sign-in-submit" disabled={disabledValue} type="submit">{buttonText}</Button> 
+                </Form>
+                <LinkSignUp>
+                    <Link to={`/signup`}>
+                        <p>First time? Create an account!</p>
+                    </Link>                
+                </LinkSignUp>                   
+            </SigninContainer>
+        </ContainerPage>
+        
+    )
 }
