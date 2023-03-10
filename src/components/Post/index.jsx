655e75fc15e2614954df4ea3285cad51d51c2402
Paula Rodrigues <pullynnhah@icloud.com>
2023-03-10 10:19:14 -0300

    :recycle: create component for likes

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -1,10 +1,9 @@
 import { ReactTagify } from "react-tagify";
 import StyledPost from "./styled";
 import Meta from "../Meta";
-import { IconContext } from "react-icons";
-import { FaRegHeart } from "react-icons/fa";
-import { RiHeartFill } from "react-icons/ri";
+
 import { useState } from "react";
+import LikeIcon from "../LikeIcon";
 export default function Post({
   id,
   content,
@@ -16,16 +15,15 @@ export default function Post({
   image,
   description
 }) {
-
   const [liked, setLiked] = useState(true);
   const [likesQty, setLikesQty] = useState(5);
 
   function changeLike() {
-    setLiked(!liked)
+    setLiked(!liked);
     if (liked) {
       setLikesQty(likesQty - 1);
     } else {
-      setLikesQty(likesQty + 1)
+      setLikesQty(likesQty + 1);
     }
   }
 
@@ -33,34 +31,7 @@ export default function Post({
     <StyledPost data-test="post">
       <div>
         <img src={pictureUrl} alt={username} />
-        
-        
-
-  {liked? 
-    <div onClick={changeLike}>
-    <IconContext.Provider
-      value={{
-        color: "#AC0000",
-        size: "20px",
-        cursor: "pointer",
-      }}>
-      <RiHeartFill />
-    </IconContext.Provider>
-    </div>
-    :
-    <div onClick={changeLike}>
-    <IconContext.Provider
-    value={{
-      color: "#FFFFFF",
-      size: "20px",
-      cursor: "pointer",
-    }}>
-    <FaRegHeart />
-  </IconContext.Provider>
-  </div>
-}
-
-
+        <LikeIcon liked={liked} changeLike={changeLike} />
         <p>{likesQty} likes</p>
       </div>
       <div>
